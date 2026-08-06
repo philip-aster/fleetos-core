f32617e71a9981791313bb67af6866e94324241e
Philip Aster <kahtona@protonmail.com>
2026-08-06 21:02:11 +0100

    added architecture

---
+++ build.rs
@@ -0,0 +1,17 @@
+// build.rs
+// Compiles Protobuf definitions in proto/v1/ into Rust code during `cargo build`
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_prost_build::configure()
+        .build_server(true) // Generate gRPC server traits
+        .build_client(true) // Generate gRPC client structs
+        .compile_protos(
+            &["proto/v1/identity.proto", "proto/v1/state.proto"],
+            &["proto/v1"], // Search path for imported proto files
+        )?;
+
+    println!("cargo:rerun-if-changed=proto/v1/identity.proto");
+    println!("cargo:rerun-if-changed=proto/v1/state.proto");
+
+    Ok(())
+}
