20bfa5173c121853e8ba2b3f065e9d5b1882b921
Philip Aster <kahtona@protonmail.com>
2026-08-10 14:13:43 +0100

    Added Library Specs

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,33 @@ pub use attestor::{AttestationPayload, HardwareAttestor};
 pub use hash::IdentityHash;
 pub use policy::{PolicyAction, SagRule};
 pub use spiffe::{EntityType, SpiffeId};
+
+// fleetos-core/src/models.rs
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum RuntimeEngine {
+    CloudHypervisor(CloudHypervisorConfig),
+    Containerd(ContainerdConfig),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CloudHypervisorConfig {
+    pub kernel_path: String,
+    pub initrd_path: Option<String>,
+    pub vcpus: u32,
+    pub memory_mb: u64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ContainerdConfig {
+    pub image: String,
+    pub snapshotter: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PodSpec {
+    pub id: String,
+    pub namespace: String,
+    pub runtime: RuntimeEngine,
+}
